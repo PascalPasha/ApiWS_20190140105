4eaf1194aadffa10ff3cb1bea806d4171aa4a78c
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2022-01-06 02:08:17 +0700

    perubahan kata yang typo/salah

--- src/main/java/webservice/create/api/pertemuan/empatbelas/c/DatabaseController.java
+++ src/main/java/webservice/create/api/pertemuan/empatbelas/c/DatabaseController.java
@@ -4,25 +4,18 @@
  */
 package webservice.create.api.pertemuan.empatbelas.c;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -34,6 +27,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/datamhs")
 public class DatabaseController {
+
+    @Autowired
+    RepositoryMahasiswa repositoryMahasiswa;
+    
+    @PostMapping(value = "/addnewmhs")
+    public Mahasiswadata addnewmhs(@RequestBody Mahasiswadata param)
+    {
+        repositoryMahasiswa.save(param);
+        return param;
+    }  
+    
+    @GetMapping(value = "/getallmhs")
+    public List<Mahasiswadata> getallmhs()
+    {
+        return repositoryMahasiswa.findAll();
+    }
+    
+    @GetMapping(value = "/getmhsbyid")
+    public Mahasiswadata getmhsbyid(@RequestParam int id)
+    {
+        return repositoryMahasiswa.findById(id).get();
+    }
+    
+    @PutMapping(value = "/updatemhs")
+    public Mahasiswadata updatemhs(@RequestBody Mahasiswadata param)
+    {
+        return repositoryMahasiswa.save(param);
+    }
+    
+    @DeleteMapping(value = "/deletemhs")
+    public List <Mahasiswadata> deletemhs(@RequestParam int id)
+    {
+        repositoryMahasiswa.deleteById(id);
+        List<Mahasiswadata> mhslist = repositoryMahasiswa.findAll();
+        return mhslist;
+    }
+}
+    
 
 //    @RequestMapping("/keloladatamhs")
 //    @ResponseBody
@@ -49,43 +80,8 @@ public class DatabaseController {
 //        
 //        return mahasiswadata;
 //    }
-    @Autowired
-    RepositoryMahasiswa repositoryMahasiswa;
-    
-    @PostMapping(value = "/addnewmhs")
-    public Mahasiswadata addnewmhs(@RequestBody Mahasiswadata param)
-    {
-        repositoryMahasiswa.save(param);
-        return param;
-    }
-    
-    @GetMapping(value = "/getallmhs")
-    public List<Mahasiswadata> getallmhs()
-    {
-        return repositoryMahasiswa.findAll();
-    }
-    
-    @GetMapping(value = "/getmhsbyid")
-    public Mahasiswadata getmhsbyid(@RequestParam int id)
-    {
-        return repositoryMahasiswa.findById(id).get();
-    }
-    
-    @PostMapping(value = "/updatemhs")
-    public Mahasiswadata updatemhs(@RequestBody Mahasiswadata param)
-    {
-        return repositoryMahasiswa.save(param);
-    }
-    
-    @GetMapping(value = "/deletemhs")
-    public List <Mahasiswadata> deletemhs(@RequestParam int id)
-    {
-        repositoryMahasiswa.deleteById(id);
-        List<Mahasiswadata> mhslist = repositoryMahasiswa.findAll();
-        return mhslist;
-    }
-}
-    
+
+
 
 //   @GetMapping("/data")
 //  public ResponseEntity<List<Mahasiswadata>> getAllTutorials(@RequestParam(required = false) String nama) {
